082d873100f754a02fbecbebe16658072e222265
JamesBaiyong <mrbaiyongde@sina.com>
2018-05-24 17:52:27 +0800

    管理员修改用户功能

---
+++ web/WeiManagerByFlask/app/manager/froms.py
@@ -0,0 +1,32 @@
+# encoding=utf-8
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, DateField, ValidationError, RadioField, IntegerField
+from wtforms.validators import DataRequired, Email, Length, Regexp, EqualTo
+from ..models import User
+
+class UsersForm(FlaskForm):
+    title = StringField(u'用户检索', validators=[DataRequired(), Length(1, 64),Email()])
+    # status = RadioField(u'是否完成', validators=[DataRequired()],  choices=[("1", u'是'),("0",u'否')])
+    submit = SubmitField(u'提交')
+
+class UsersChangeForm(FlaskForm):
+    name = StringField(u'用户名修改', validators=[DataRequired(), Length(1, 64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,u'用户名必须是英文小写,数字,句号,或则下划线')])
+    user_email = StringField(u'用户邮箱', validators=[DataRequired(), Length(1, 64),Email()])
+    password = PasswordField(u'密码', validators=[
+        DataRequired(), EqualTo('password2', message=u'两次密码不相同')])
+    password2 = PasswordField(u'确认密码', validators=[DataRequired()])
+    b_number = IntegerField(u'借阅数：')
+    b_status = StringField(u'借阅情况：')
+    b_date = DateField(u'借阅时间')
+    r_date = DateField(u'归还时间')
+    cost = IntegerField(u'资费')
+    submit = SubmitField(u'更新用户状态')
+
+    # 以validate_开头后面跟字段名,做检验
+    def validate_user_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(u'此邮箱已存在')
+
+    def validate_name(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError(u'此用户已存在')
--- web/WeiManagerByFlask/app/manager/views.py
+++ web/WeiManagerByFlask/app/manager/views.py
@@ -1,19 +1,57 @@
 #encoding=utf-8
 
-from flask import render_template
-from flask_login import login_required
+from flask import render_template, flash, url_for, redirect
+from flask_login import login_required, current_user
 from ..models import *
 from . import manager
+from froms import UsersForm, UsersChangeForm
 
 @manager.route('/manager_index')
 @login_required
 def manger_index():
     return render_template('manager/index.html')
 
-@manager.route('/manager_users')
+@manager.route('/manager_users',methods=['GET', 'POST'])
 @login_required
 def manger_users():
-    return render_template('manager/manager_users.html')
+    form = UsersForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.title.data).first()
+        if user:
+            flash(u'用户信息如下,请谨慎操作')
+            user_info = User.query.filter_by(id=user.id).first_or_404()
+            return render_template('manager/manager_users.html', form=form, user_info=user_info)
+        flash(u'数据库中不存在该用户')
+    return render_template('manager/manager_users.html',form=form)
+
+@manager.route('/change_user/<user_id>',methods=['GET', 'POST'])
+@login_required
+def chage_user(user_id):
+    # 增加角色判断,以防普通用户对别的用户做修改
+    if current_user.role_id != 1:
+        flash(u'很抱歉,只有管理员才能访问相应页面')
+        return redirect(url_for('main.index'))
+
+    form = UsersChangeForm()
+        # 按照查询出的用户id去修改用户信息,确保修改的用户为特定用户
+    user = User.query.filter_by(id=int(user_id)).first_or_404()
+    u = User()
+    if form.validate_on_submit():
+        user.email=form.user_email.data,
+        user.username=form.name.data,
+        # 需要实例化用户表,调用计算password的hash方法
+        u.password = form.password.data
+        user.password_hash=u.password_hash,
+
+        user.borrownum=form.b_number.data,
+        user.borrowing=form.b_status.data,
+        user.borrowTime=form.b_date.data,
+        user.returnTime=form.r_date.data,
+        user.cost=form.cost.data
+        db.session.commit()
+        flash(u'你已经成功更新用户所有信息')
+        return render_template('manager/index.html')
+    return render_template('manager/change_users.html', form=form, user=user)
 
 @manager.route('/manager_notice')
 @login_required
