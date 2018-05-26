0badc21ae826f96bc97a024be3506a81d3ec1ebb
JamesBaiyong <mrbaiyongde@sina.com>
2018-05-27 00:21:02 +0800

    图书检索

--- web/WeiManagerByFlask/app/docinfo/froms.py
+++ web/WeiManagerByFlask/app/docinfo/froms.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
 from flask_wtf import FlaskForm
 from wtforms import RadioField, SubmitField, StringField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 class BookListForm(FlaskForm):
-    title = StringField('标题', validators=[DataRequired(), Length(1, 64)])
-    status = RadioField('是否完成', validators=[DataRequired()],  choices=[("1", '是'),("0",'否')])
-    submit = SubmitField('提交')
+    title = StringField(u'标题', validators=[DataRequired(), Length(1, 64)])
+    submit = SubmitField(u'检索')
--- web/WeiManagerByFlask/app/docinfo/views.py
+++ web/WeiManagerByFlask/app/docinfo/views.py
@@ -1,15 +1,22 @@
 #encoding=utf-8
-from flask import render_template, request
+from flask import render_template, flash
 from froms import BookListForm
 from . import doc
 from ..models import BookInfo, PaperInfo, DegreeInfo
 
-@doc.route('/book_info')
+@doc.route('/book_info', methods=['GET', 'POST'])
 def booksinfo():
     # 图书信息
-
+    form = BookListForm()
     books = BookInfo.query.all()
-    return render_template('docinfo/bookinfo.html', booklists=books)
+    if form.validate_on_submit():
+        book = BookInfo.query.filter_by(book_name=form.title.data).first()
+        if book:
+            flash(u'已找到%s'%book.book_name)
+            book = BookInfo.query.filter_by(id=book.id).first_or_404()
+            return render_template('docinfo/bookinfo.html', book=book, form=form)
+        flash(u'暂无%s'%form.title.data)
+    return render_template('docinfo/bookinfo.html', booklists=books, form=form)
 
 @doc.route('/paper_info')
 def paperinfo():
