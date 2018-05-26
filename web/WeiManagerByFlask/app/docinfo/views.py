73f8ea924ad43039c8c8aa60cc21f14ac4f9f087
JamesBaiyong <mrbaiyongde@sina.com>
2018-05-27 00:40:35 +0800

    论文检索

--- web/WeiManagerByFlask/app/docinfo/views.py
+++ web/WeiManagerByFlask/app/docinfo/views.py
@@ -18,16 +18,30 @@ def booksinfo():
         flash(u'暂无%s'%form.title.data)
     return render_template('docinfo/bookinfo.html', booklists=books, form=form)
 
-@doc.route('/paper_info')
+@doc.route('/paper_info', methods=['GET', 'POST'])
 def paperinfo():
     # 期刊论文信息
+    form = BookListForm()
+    papers = PaperInfo.query.all()
+    if form.validate_on_submit():
+        paper = PaperInfo.query.filter_by(paper_title=form.title.data).first()
+        if paper:
+            flash(u'已找到%s' % paper.paper_title)
+            paper = PaperInfo.query.filter_by(id=paper.id).first_or_404()
+            return render_template('docinfo/paperinfo.html', paper=paper, form=form)
+        flash(u'暂无%s' % form.title.data)
+    return render_template('docinfo/paperinfo.html', paperlists=papers, form=form)
 
-    paper = PaperInfo.query.all()
-    return render_template('docinfo/paperinfo.html', paperlists=paper)
-
-@doc.route('/degree_info')
+@doc.route('/degree_info', methods=['GET', 'POST'])
 def degreeinfo():
     # 期刊论文信息
-
-    degree = DegreeInfo.query.all()
-    return render_template('docinfo/degreeinfo.html', degreelists=degree)
+    form = BookListForm()
+    degrees = DegreeInfo.query.all()
+    if form.validate_on_submit():
+        degree = DegreeInfo.query.filter_by(degree_title=form.title.data).first()
+        if degree:
+            flash(u'已找到%s' % degree.degree_title)
+            degree = DegreeInfo.query.filter_by(id=degree.id).first_or_404()
+            return render_template('docinfo/degreeinfo.html', degree=degree, form=form)
+        flash(u'暂无%s' % form.title.data)
+    return render_template('docinfo/degreeinfo.html', degreelists=degrees, form=form)
